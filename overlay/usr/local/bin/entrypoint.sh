28a55c89ef551c739db5e9d74f8f5a8147c7cb89
Carlo Landmeter <clandmeter@alpinelinux.org>
2019-05-28 21:34:56 +0000

    entrypoint: add missing nginx ssl header

--- overlay/usr/local/bin/entrypoint.sh
+++ overlay/usr/local/bin/entrypoint.sh
@@ -125,6 +125,7 @@ nginx_config() {
 
 	    proxy_set_header    Host                \$http_host;
 	    proxy_set_header    X-Real-IP           \$remote_addr;
+	    proxy_set_header    X-Forwarded-Ssl      on;
 	    proxy_set_header    X-Forwarded-For     \$proxy_add_x_forwarded_for;
 	    proxy_set_header    X-Forwarded-Proto   \$scheme;
 	    proxy_set_header    Upgrade             \$http_upgrade;
