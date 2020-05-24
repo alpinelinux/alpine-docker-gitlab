da73c820c16eca250177e74c163d4e4f8b3058a2
Carlo Landmeter <clandmeter@alpinelinux.org>
2020-05-25 00:15:49 +0200

    ruby: protobuf including missing protos

--- overlay/tmp/protobuf/build.sh
+++ overlay/tmp/protobuf/build.sh
@@ -2,17 +2,22 @@
 
 set -euo pipefail
 
-mkdir -p /tmp/protobuf
+mkdir -p /tmp/protobuf/src
 
 wget -qO- https://github.com/protocolbuffers/protobuf/releases/download/v"$PROTOBUF_VERSION"/protobuf-ruby-"$PROTOBUF_VERSION".tar.gz |
-	tar --strip-components=1 -z -x -C /tmp/protobuf
+	tar --strip-components=1 -z -x -C /tmp/protobuf/src
 
-cd /tmp/protobuf/ruby/
+cd /tmp/protobuf/src
 
 for patch in ../*.patch; do
-    patch -p0 -i "$patch"
+	patch -p0 -i "$patch"
 done
+
+./configure && make
+
+cd /tmp/protobuf/src/ruby/
+
+rake genproto
 
 gem build google-protobuf.gemspec
 gem install --verbose google-protobuf-"$PROTOBUF_VERSION".gem
-
