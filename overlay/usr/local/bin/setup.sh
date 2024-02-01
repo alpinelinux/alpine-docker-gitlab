7168f463d1c2cce3d3d802c4e12cdfb789b18db3
Kevin Daudt <kdaudt@alpinelinux.org>
2024-02-01 16:55:07 +0100

    gitlab: apply patch to fix navbar height when not logged in
    When using a custom logo in gitlab 16.7, the logo expands the navbar
    when a user is logged in. An MR[0] was made upstream to fix it.
    
    Add the MR as a patch to fix the navbar.
    
    [0]: https://gitlab.com/gitlab-org/gitlab/-/merge_requests/143012

--- overlay/usr/local/bin/setup.sh
+++ overlay/usr/local/bin/setup.sh
@@ -115,6 +115,7 @@ apply_patch -p0 -i /tmp/gitlab/puma-socket-path.patch
 apply_patch -p1 -i /tmp/gitlab/cable-defaults.patch
 apply_patch -p1 -i /tmp/gitlab/database-remove-geo.patch
 apply_patch -p1 -i /tmp/gitlab/ruby-3.1-compat.patch
+apply_patch -p1 -i /tmp/gitlab/fix-navbar-height.patch
 apply_patch -p0 -i /tmp/logrotate/logrotate-defaults.patch
 apply_patch -p0 -i /tmp/nginx/nginx-config.patch
 apply_patch -p1 -i /tmp/resque/resque-config.patch
