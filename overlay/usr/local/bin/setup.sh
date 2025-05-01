fb4294a9c0e77d22c5eeed521eeae28ddfaaf81c
Kevin Daudt <kdaudt@alpinelinux.org>
2025-05-01 15:26:57 +0200

    gitlab: upgrade to 17.10.5

--- overlay/usr/local/bin/setup.sh
+++ overlay/usr/local/bin/setup.sh
@@ -117,6 +117,7 @@ apply_patch -p0 -i /tmp/gitlab/puma-socket-path.patch
 apply_patch -p1 -i /tmp/gitlab/cable-defaults.patch
 apply_patch -p1 -i /tmp/gitlab/database-remove-geo.patch
 apply_patch -p1 -i /tmp/gitlab/gemfile-install-rust-gems-separately.patch
+apply_patch -p1 -i /tmp/gitlab/gemfile-grpc-1.71.0.patch
 apply_patch -p0 -i /tmp/logrotate/logrotate-defaults.patch
 apply_patch -p0 -i /tmp/nginx/nginx-config.patch
 apply_patch -p1 -i /tmp/resque/resque-config.patch
