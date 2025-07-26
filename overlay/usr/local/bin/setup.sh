d936e688e5e2fe7191b0c9ab4e8b57cd068ed167
Kevin Daudt <kdaudt@alpinelinux.org>
2025-07-26 10:22:58 +0200

    gitlab: upgrade to 18.1.2

--- overlay/usr/local/bin/setup.sh
+++ overlay/usr/local/bin/setup.sh
@@ -120,7 +120,7 @@ apply_patch -p1 -i /tmp/gitlab/database-remove-geo.patch
 apply_patch -p1 -i /tmp/gitlab/gemfile-install-rust-gems-separately.patch
 apply_patch -p1 -i /tmp/gitlab/gemfile-grpc-1.71.0.patch
 apply_patch -p0 -i /tmp/logrotate/logrotate-defaults.patch
-apply_patch -p0 -i /tmp/nginx/nginx-config.patch
+apply_patch -p1 -i /tmp/nginx/nginx-config.patch
 apply_patch -p1 -i /tmp/resque/resque-config.patch
 
 # temporary symlink the example configs to make setup happy
