4c058a89b877fa94bd887219bb2713c7f8ef2abd
Kevin Daudt <kdaudt@alpinelinux.org>
2026-07-15 22:22:37 +0200

    gitlab: upgrade to 19.0.4

--- overlay/usr/local/bin/setup.sh
+++ overlay/usr/local/bin/setup.sh
@@ -141,8 +141,6 @@ cd "$gitlab_location"
 export MAKEFLAGS='-j1'
 bundle install
 
-apply_patch -d /usr/local/bundle/gems/sys-filesystem*/ -p1 -i /tmp/gitlab/gem-sys-filesystem-no-lfs64.patch
-
 ###################
 ## gitlab-workhorse
 ###################
